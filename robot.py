897eac6e90b455906e328344987cdbcc24d1a7ed
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-26 15:14:41 +0200

    Added discount for prod. over 500 kr

--- robot.py
+++ robot.py
@@ -2,13 +2,19 @@
 # Nummer 8, classes + init + loop för product så jag slipper def allt i total_price
 # Varje product def i listan products. Tillhör även klassen Product.
 
+#La till rabatt när en produkt överskrider 500 kr
+
 class Product:
 	def __init__(self, price, count, tax):
 		self.price = price
 		self.count = count
 		self.tax = tax
 	def price_with_tax(self):
-		return self.price * self.count * self.tax
+		total = self.price * self.count *self.tax
+		if total > 500:
+			return 0.9 * total #här läggs rabatten på 10% till
+		else: 
+			return total 
 
 products = [Product (price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
 
