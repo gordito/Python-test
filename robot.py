cd43eb209435e8054777138d136106731f37a62a
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-26 12:17:50 +0200

    Test of dic instead

--- robot.py
+++ robot.py
@@ -1,9 +1,8 @@
-robot_price = 900
-robot_count = 2
-robot_tax = 1.25
-book_price = 100
-book_count = 1
-book_tax = 1.06
+robot = {
+	"price":900, 
+	"count":2, 
+	"tax": 1.25,
+}
 
-print(robot_price * robot_count * robot_tax + book_price * book_count * book_tax)
+print(robot["price"] * 2 * 1.25 + 100 *1.06)
 
