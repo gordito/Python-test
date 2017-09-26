49b49eb6112c66a6d185bf8c06d7f5d1e6ae4d3f
Robin Kochauf <robin@gordito.se>
2017-09-26 18:09:16 +0200

    Updated with PDF support

--- robot.py
+++ robot.py
@@ -32,11 +32,31 @@ Product(price=20, count=1, tax=1.25, name="Ice Cream"),
 
 total_price = 0
 
+# IMPORT PDF EDITOR
+from fpdf import FPDF
+
+# CREATE NEW PDF AND ADD A PAGE
+pdf = FPDF()
+pdf.add_page()
+
+# SET FONT TO HEADER SIZE
+pdf.set_font('Arial', 'B', 16)
+
+# PRINT A HEADER
+pdf.cell(40, 10, 'Sannas kvitto', 0, 1)
+
+# SET FONT TO NORMAL SIZE
+pdf.set_font('Arial', '', 12)
 
 for product in products:
 	print("Produkt: {:10} Antal: {:<10} Pris: {:>7} kr".format(product.name, product.count, product.price_with_tax()))
+	# ADD THE SAME LINE TO THE PDF FILE
+	pdf.cell(40, 10, "Produkt: {:10} Antal: {:<10} Pris: {:>7} kr".format(product.name, product.count, product.price_with_tax()), 0, 1)
 
 print("-"*80) # streckad linje
+
+# ADD THE STRECKAD LINJE TO THE PDF AS WELL
+pdf.cell(40, 10, "-"*80, 0, 1)
 
 total_tax = 0
 
@@ -48,9 +68,17 @@ for product in products:
 	total_price = total_price + product.price_with_tax()
 print("\nTotalsumma: {:>39} kr".format(total_price))
 
+# ADD THE TOTALSUMME TO THE PDF
+pdf.cell(40, 10, "\nTotalsumma: {:>39} kr".format(total_price), 0, 1)
+
 #line break \n
 #Måsvingar där formateringen och variabeln ska in. 7 står för avståndet. Blir som en formaterad klass.
 
 
 print("Varav moms: {:>39} kr".format(total_tax))
 
+# AND ADD THE THE MOMS TO THE PDF
+pdf.cell(40, 10, "\nTotalsumma: {:>39} kr".format(total_price), 0, 1)
+
+# SAVE THE PDF TO DISK
+pdf.output('sannas_fina_kvitto.pdf', 'F')  # SAVE PDF TO DISK
