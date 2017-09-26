cd5a0e8fd8b971faa6759ef35a2f98e98ebdfec4
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-26 16:36:08 +0200

    Right align numbers and print out string with description

--- robot.py
+++ robot.py
@@ -5,25 +5,30 @@
 #La till rabatt när en produkt överskrider 500 kr
 
 class Product:
-	def __init__(self, price, count, tax):
+	def __init__(self, price, count, tax, name):
 		self.price = price
 		self.count = count
 		self.tax = tax
+		self.name = name
 	def price_with_tax(self):
-		total = self.price * self.count *self.tax
+		total = self.price * self.count * self.tax 
 		if total > 500:
 			return 0.9 * total #här läggs rabatten på 10% till
 		else: 
 			return total #annars ingen rabatt
 
 products = [
-Product (price=900, count=2, tax=1.25), 
-Product(price=100, count=1, tax=1.06),
-Product(price=20, count=1, tax=1.25),
+Product(price=900, count=2, tax=1.25, name="Robot"), 
+Product(price=100, count=1, tax=1.06, name="Book"),
+Product(price=20, count=1, tax=1.25, name="Ice Cream"),
 ]
 
 total_price = 0
 
 for product in products:
 	total_price = total_price + product.price_with_tax()
-print(total_price)
+print("Summa: {:>7} kr".format(total_price))
+
+for product in products:
+	print("Produkten heter {} och kostar: {:>7} kr.".format(product.name, product.price_with_tax()))
+
